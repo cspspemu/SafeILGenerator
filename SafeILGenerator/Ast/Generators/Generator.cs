be6abe01a46a49a7aed11b116675adac4cb999d1
soywiz <soywiz@gmail.com>
2012-12-12 05:21:13 +0100

    - Added code to generate delegates for fast calling methods - It seems that it is slower than Invoke (?)

--- SafeILGenerator/Ast/Generators/Generator.cs
+++ SafeILGenerator/Ast/Generators/Generator.cs
@@ -1,8 +1,13 @@
-﻿using SafeILGenerator.Ast.Nodes;
+﻿//#define USE_STATIC_REFERENCE
+#define USE_NORMAL_INVOKE
+
+using SafeILGenerator.Ast.Nodes;
+using SafeILGenerator.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +15,29 @@ namespace SafeILGenerator.Ast.Generators
 {
 	public abstract class Generator<TGenerator>
 	{
+#if !USE_NORMAL_INVOKE
+#if USE_STATIC_REFERENCE
+		delegate void MapDelegate(AstNode AstNode);
+		private ILInstanceHolderPoolItem StaticThisReference;
+#else
+		delegate void MapDelegate(object This, AstNode AstNode);
+#endif
+#endif
+
+#if USE_NORMAL_INVOKE
 		private Dictionary<Type, MethodInfo> GenerateMappings = new Dictionary<Type, MethodInfo>();
+#else
+		private Dictionary<Type, MapDelegate> GenerateMappings = new Dictionary<Type, MapDelegate>();
+#endif
 
 		public Generator()
 		{
+#if !USE_NORMAL_INVOKE
+#if USE_STATIC_REFERENCE
+			this.StaticThisReference = ILInstanceHolder.Alloc(this.GetType(), this);
+#endif
+#endif
+
 			foreach (
 				var Method
 				in
@@ -23,11 +47,48 @@ namespace SafeILGenerator.Ast.Generators
 					.Where(Method => Method.GetParameters().Count() == 1)
 			)
 			{
-				GenerateMappings[Method.GetParameters().First().ParameterType] = Method;
+				GenerateMappings[Method.GetParameters().First().ParameterType] = GenerateInvoke(Method);
 			}
 
 			this.Reset();
 		}
+
+		static Generator<GeneratorCSharp> Test;
+
+		private void MyMethod(AstNode AstNode)
+		{
+			Test.MyMethod(AstNode);
+		}
+
+#if USE_NORMAL_INVOKE
+		private MethodInfo GenerateInvoke(MethodInfo MethodInfo)
+		{
+			return MethodInfo;
+		}
+#else
+		private MapDelegate GenerateInvoke(MethodInfo MethodInfo)
+		{
+
+			var DM = new DynamicMethod("GenerateInvoke_" + MethodInfo.Name, typeof(void), new Type[] { typeof(object), typeof(AstNode) }, this.GetType());
+			var ILGenerator = DM.GetILGenerator();
+			
+#if USE_STATIC_REFERENCE
+			ILGenerator.Emit(OpCodes.Ldsfld, StaticThisReference.FieldInfo);
+#else
+			ILGenerator.Emit(OpCodes.Ldarg_0);
+			ILGenerator.Emit(OpCodes.Castclass, this.GetType());
+#endif
+
+			ILGenerator.Emit(OpCodes.Ldarg_1);
+			ILGenerator.Emit(OpCodes.Castclass, MethodInfo.GetParameters()[0].ParameterType);
+			ILGenerator.Emit(OpCodes.Call, MethodInfo);
+			if (MethodInfo.ReturnType != typeof(void)) ILGenerator.Emit(OpCodes.Pop);
+
+			ILGenerator.Emit(OpCodes.Ret);
+
+			return (MapDelegate)DM.CreateDelegate(typeof(MapDelegate));
+		}
+#endif
 
 		public virtual TGenerator Reset()
 		{
@@ -57,7 +118,15 @@ namespace SafeILGenerator.Ast.Generators
 				}
 				throw (new NotImplementedException(String.Format("Don't know how to generate {0} for {1}", AstNodeType, this.GetType())));
 			}
+#if USE_NORMAL_INVOKE
 			GenerateMappings[AstNodeType].Invoke(this, new object[] { AstNode });
+#else
+#if USE_STATIC_REFERENCE
+			GenerateMappings[AstNodeType](AstNode);
+#else
+			GenerateMappings[AstNodeType](this, AstNode);
+#endif
+#endif
 		}
 	}
 }
